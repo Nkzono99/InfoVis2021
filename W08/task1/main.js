1487b0a4d79fab8384f13afeb4421eba3ee293ed
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2021-05-12 14:04:17 +0900

    Add: W08のTask1の追加

---
+++ W08/task1/main.js
@@ -0,0 +1,26 @@
+var data = [
+      { label: 'Apple', value: 100 },
+      { label: 'Banana', value: 200 },
+      { label: 'Cookie', value: 50 },
+      { label: 'Doughnut', value: 120 },
+      { label: 'Egg', value: 80 },
+];
+
+d3.csv("https://Nkzono99.github.io/InfoVis2021/W08/task1/data.csv")
+      .then(data => {
+            var config = {
+                  parent: '#drawing_region',
+                  width: 256,
+                  height: 128,
+                  margin: { top: 10, right: 10, bottom: 20, left: 60 },
+                  xticks: 10,
+                  yticks: 10,
+                  padrate: 0.2,
+            };
+
+            let barChart = new BarChart(config, data);
+            barChart.update();
+      })
+      .catch(error => {
+            console.log(error);
+      });
