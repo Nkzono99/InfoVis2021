0c06ef1ccbec2d25f5ae61af59cb9b5771af7521
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2021-06-11 01:41:16 +0900

    Update: dtw.json

--- final/create_data.py
+++ final/create_data.py
@@ -12,19 +12,19 @@ names = df['都道府県名'].unique()  # 都道府県名リスト
 def nkansen_for_dtw(name):
     result = df[df['都道府県名'] == name]['各地の感染者数_1日ごとの発表数']
     rmin, rmax = result.min(), result.max()
-    result = result / (rmax - rmin) - rmin
+    result = (result - rmin) / (rmax - rmin)
     return result
 
 dists = defaultdict(dict)
 for name1, name2 in combinations(names, 2):
-    # dtw = fastdtw(nkansen_for_dtw(name1), nkansen_for_dtw(name2))[0]
-    dtw = abs(nkansen_for_dtw(name1).values - nkansen_for_dtw(name2).values)**2
+    dtw = fastdtw(nkansen_for_dtw(name1), nkansen_for_dtw(name2))[0]
+    # dtw = abs(nkansen_for_dtw(name1).values - nkansen_for_dtw(name2).values)**2
     dtw = dtw.mean()
     print(name1, name2, dtw)
     dists[name1][name2] = dtw
     dists[name2][name1] = dtw
 
-with open('dtw.json', 'w', encoding='utf-8') as f:
+with open('dist.json', 'w', encoding='utf-8') as f:
     json.dump(dists, f)
 #### Created DTW Dataset #####
 
